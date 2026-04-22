8ca02d051df874e83f3eb0e0ec0e576624fd1b04
ParisJibron <parisjibron.240604075@student.hamzanwadi.ac.id>
2026-04-22 20:39:09 +0800

    menambahkan file 9.operator-aritmatika.php

---
+++ 9.operator-aritmatika.php
@@ -0,0 +1,19 @@
+<?php
+
+$var1 = 10; 
+$var2 = 20;
+
+// penjumlahan
+$hasil = $var1 + $var2;
+echo $hasil;
+echo "\n";
+
+// pengurangan
+$hasil2 = $var1 - $var2;
+echo $hasil2;
+echo "\n";
+
+// tugas
+// perkalian
+// pembagian
+// modulus
